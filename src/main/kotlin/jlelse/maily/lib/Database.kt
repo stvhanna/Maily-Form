5361976af3cbc42ca3c803552df96b9306c5fcdc
Jan-Lukas Else <mail@jlelse.de>
2018-10-06 10:09:43 +0200

    Fix DB migration

--- src/main/kotlin/jlelse/maily/lib/Database.kt
+++ src/main/kotlin/jlelse/maily/lib/Database.kt
@@ -18,31 +18,39 @@ object Database {
     }
 
     private fun createAndMigrate() {
+        val latestVersion = 1
         // Create Database if it doesn't exist yet
-        db.run("CREATE TABLE IF NOT EXISTS submissions (id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER, formName TEXT, replyTo TEXT, text TEXT, response TEXT, status INTEGER)") { err ->
+        db.get("PRAGMA schema_version") { err, row ->
             if (err != null) console.log(err.message as? String)
             else {
-                console.log("Database initialized")
-                db.get("PRAGMA user_version") { err2, row ->
-                    if (err2 != null) console.log(err2.message as? String)
-                    else {
-                        // Check and migrate Database
-                        val version = row.user_version
-                        val latestVersion = 1
-                        console.log("Current DB version: $version")
-                        var migrationStatement = ""
-                        // Version 1 changes sent to status and adds response text
-                        if (version < 1) migrationStatement += """
-                            ALTER TABLE submissions RENAME TO tmp_submissions;
-                            CREATE TABLE submissions (id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER, formName TEXT, replyTo TEXT, text TEXT, response TEXT, status INTEGER);
-                            INSERT INTO submissions(id, time, formName, replyTo, text, status) SELECT id, time, formName, replyTo, text, sent FROM tmp_submissions;
-                            DROP TABLE tmp_submissions;
-                        """.trimIndent()
-                        if (!migrationStatement.isBlank()) {
-                            console.log("Migrating DB to latest version.")
-                            db.exec("BEGIN TRANSACTION;${migrationStatement}PRAGMA user_version = $latestVersion;COMMIT;") { err3 ->
-                                if (err3 != null) console.log(err3.message as? String)
-                                else console.log("Migration finished.")
+                val schemaVersion = row.schema_version
+                if (schemaVersion == 0) {
+                    console.log("Create Database")
+                    db.exec("CREATE TABLE IF NOT EXISTS submissions (id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER, formName TEXT, replyTo TEXT, text TEXT, response TEXT, status INTEGER);PRAGMA user_version = $latestVersion;") { err2 ->
+                        if (err2 != null) console.log(err2.message as? String)
+                        else console.log("Database created")
+                    }
+                } else {
+                    db.get("PRAGMA user_version") { err2, row2 ->
+                        if (err2 != null) console.log(err2.message as? String)
+                        else {
+                            // Check and migrate Database
+                            val userVersion = row2.user_version
+                            console.log("Current DB version: $userVersion")
+                            var migrationStatement = ""
+                            // Version 1 changes sent to status and adds response text
+                            if (userVersion < 1) migrationStatement += """
+                                ALTER TABLE submissions RENAME TO tmp_submissions;
+                                CREATE TABLE submissions (id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER, formName TEXT, replyTo TEXT, text TEXT, response TEXT, status INTEGER);
+                                INSERT INTO submissions(id, time, formName, replyTo, text, status) SELECT id, time, formName, replyTo, text, sent FROM tmp_submissions;
+                                DROP TABLE tmp_submissions;
+                            """.trimIndent()
+                            if (!migrationStatement.isBlank()) {
+                                console.log("Migrating DB to latest version.")
+                                db.exec("BEGIN TRANSACTION;${migrationStatement}PRAGMA user_version = $latestVersion;COMMIT;") { err3 ->
+                                    if (err3 != null) console.log(err3.message as? String)
+                                    else console.log("Migration finished.")
+                                }
                             }
                         }
                     }
